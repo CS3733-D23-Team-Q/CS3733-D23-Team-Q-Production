25894bbc33d9902483ac588fdaaa77f146c193fe
SophiaJohnn <sophiasunnyjohn@gmail.com>
2023-04-09 16:47:48 -0400

    working on edit profile

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ProfilePageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ProfilePageController.java
@@ -64,7 +64,8 @@ public class ProfilePageController {
 
     this.Username_Display.setText(username);
 
-
+    //    this.ProfilePage_Edit_Button.setOnMouseClicked(
+    //        event -> Navigation.navigate(Screen.EDIT_PROFILE));
     this.ProfilePage_Home_Button.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
   }
 
