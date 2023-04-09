350cb4c7bbf36bbfbe4e9ddb1f2da59d7a707965
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-09 17:43:17 -0400

    elevator countermeasure

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Star.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Star.java
@@ -21,9 +21,13 @@ public class Star extends Edge {
     ArrayList<Node> closedList = new ArrayList<Node>();
     ArrayList<Node> openList = new ArrayList<Node>();
     ArrayList<Node> deadList = new ArrayList<Node>();
+    boolean noElevators = true;
     String targetFloor = target.getFloor();
+    if (!targetFloor.equalsIgnoreCase(start.getFloor())) {
+      noElevators = false;
+    }
     openList.add(start);
-    while (openList.isEmpty() == false) {
+    while (!openList.isEmpty()) {
       if (openList.get(0).equals(target)) {
         closedList.add(openList.get(0)); // line to add current node to closed list
         return closedList;
@@ -46,17 +50,28 @@ public class Star extends Edge {
         /*  if (!thisEdge.getEndNode().getFloor().equalsIgnoreCase(targetFloor)) {
           continue;
         }*/
-        int relativeXDist = Math.abs(thisEdge.getStartNode().getXCoord() - target.getXCoord());
-        int relativeYDist = Math.abs(thisEdge.getStartNode().getYCoord() - target.getYCoord());
+        int relativeXDist = Math.abs(thisEdge.getEndNode().getXCoord() - target.getXCoord());
+        int relativeYDist = Math.abs(thisEdge.getEndNode().getYCoord() - target.getYCoord());
         relativeWeight =
             (int) Math.sqrt(relativeXDist * relativeXDist + relativeYDist * relativeYDist);
         if (relativeWeight < bestWeight
+            && thisEdge.getEndNode().getFloor().equalsIgnoreCase(targetFloor)
             && !deadList.contains(thisEdge.getEndNode())
             && !closedList.contains(thisEdge.getEndNode())) {
           bestWeight = relativeWeight;
           bestEdge = thisEdge;
+          System.out.println();
           System.out.println("I picked node " + bestEdge.getEndNode());
+          System.out.println();
           System.out.println("The current node has edges" + currentNode.getEdges());
+          System.out.println();
+          System.out.println("And the target node was " + target);
+          System.out.println();
+          if (closedList.size() != 0) {
+            System.out.println(
+                "And the closed list contains node " + closedList);
+          }
+          System.out.println();
         }
       }
       if (currentNode.getEdges().size() == 0) {
@@ -69,13 +84,15 @@ public class Star extends Edge {
       }*/
       // I need a fallback process that gets any untraversed node if no good node has been found, to
       // avoid an infinite loop of two nodes choosing each other
-      for (Edge chosen : currentNode.getEdges()) {
-        if (!closedList.contains(chosen)) {
-          bestEdge = chosen;
+      if (bestEdge == null) {
+        for (Edge chosen : currentNode.getEdges()) {
+          if (!closedList.contains(chosen.getEndNode())
+              && chosen.getEndNode().getFloor().equalsIgnoreCase(targetFloor)) {
+            bestEdge = chosen;
+          }
         }
       }
-      closedList.add(
-          bestEdge.getStartNode()); // at this point, the best weight has been found, now we need to
+      closedList.add(currentNode); // at this point, the best weight has been found, now we need to
       // match the edge
       openList.remove(currentNode);
       openList.add(bestEdge.getEndNode());
