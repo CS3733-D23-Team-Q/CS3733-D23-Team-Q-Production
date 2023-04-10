73475af036d76383b13f79a06528c77f349249e5
Kelu Liu <keluliu5@gmail.com>
2023-04-10 02:44:02 -0400

    added resizable and zoomable

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Main.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Main.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.D23.teamQ;
 
+import edu.wpi.cs3733.D23.teamQ.Pathfinding.Star;
 import edu.wpi.cs3733.D23.teamQ.Pathfinding.Star2;
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
@@ -11,61 +12,23 @@ public class Main {
     Qdb.getInstance();
     Qdb qdb = Qdb.getInstance();
     App.launch(App.class, args);
-    //    Node ns = qdb.retrieveNode(100);
-    //    Node ne = qdb.retrieveNode(130);
-    //    List<Node> nodes = Star2.aStar(ns, ne);
-    //    System.out.println(nodes.size());
-    //    for (Node n : nodes) {
-    //      System.out.println(n.getYCoord());
-    //      // System.out.println(n.getXCoord());
-    //    }
-    /*
-    System.out.println(Star2.heuristic(qdb.retrieveNode(100), qdb.retrieveNode(125)));
-    System.out.println(ns.equals(ne));
-    if (ns.getParent() == null) {
-      System.out.println("null");
-    }
-    System.out.println(qdb.retrieveAllNodes().size());
-    System.out.println(qdb.getEdges(2315).size());
-    Star2.addEdges(ns);
-    List<Edge> edges = ns.getEdges();
-    for (Edge e : edges) {
-      System.out.println(e.getEdgeID());
-      Node n = e.getEndNode();
+    Node ns = qdb.retrieveNode(100);
+    Node ne = qdb.retrieveNode(250);
+    List<Node> nodes = Star2.aStar(ns, ne);
+    System.out.println(nodes.size());
+    for (Node n : nodes) {
       System.out.println(n.getNodeID());
+      // System.out.println(n.getYCoord());
+      // System.out.println(n.getXCoord());
     }
-    System.out.println(ns.getF());
-     */
-    Node start = qdb.retrieveNode(100);
-    Node target = qdb.retrieveNode(130);
-    List<Node> path = Star2.aStar(start, target);
-    for (int i = path.size() - 1; i >= 1; i--) {
-      Node n = path.get(i);
-      Node next = path.get(i - 1);
-      int x1 = n.getXCoord();
-      int y1 = n.getYCoord();
-      int x2 = next.getXCoord();
-      int y2 = next.getYCoord();
-      // map.toBack();
+    List<Node> nodes2 = Star.aStar(ns, ne);
+    System.out.println(nodes2.size());
+    for (Node n : nodes2) {
+      System.out.println(n.getNodeID());
+      // System.out.println(n.getYCoord());
+      // System.out.println(n.getXCoord());
     }
-    /*
-    Node start = qdb.retrieveNode(100);
-    Node target = qdb.retrieveNode(130);
-    List<Node> path = Star2.aStar(start, target);
-    for (int i = path.size() - 1; i >= 1; i--) {
-      Node n = path.get(i);
-      Node next = path.get(i - 1);
-      int x1 = n.getXCoord();
-      int y1 = n.getYCoord();
-      int x2 = next.getXCoord();
-      int y2 = next.getYCoord();
-      System.out.println(y1);
-      System.out.println(y2);
-      // map.toBack();
-    }
-     */
   }
+}
 
   // shortcut: psvm
-
-}
--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
@@ -1,68 +1,67 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
 import edu.wpi.cs3733.D23.teamQ.Alert;
-import edu.wpi.cs3733.D23.teamQ.App;
 import edu.wpi.cs3733.D23.teamQ.Pathfinding.Star2;
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
 import java.io.IOException;
 import java.util.List;
+import javafx.animation.Interpolator;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
-import javafx.stage.Stage;
+import javafx.util.Duration;
+import net.kurobako.gesturefx.*;
 
 public class PathfindingController {
   Qdb qdb = Qdb.getInstance();
-  Stage stage = App.getPrimaryStage();
+  // Stage stage = App.getPrimaryStage();
   Alert alert = new Alert();
   @FXML Pane parent;
-  // @FXML HBox parent;
-  // @FXML HBox root;
+  /*
+  @FXML HBox parent;
   @FXML ImageView map;
+   */
 
   @FXML
   public void initialize() throws IOException {
-    /*
+    ImageView view = new ImageView(getClass().getResource("/L1.png").toExternalForm());
+    view.setFitWidth(1000);
+    view.setFitHeight(680);
+    javafx.scene.Node node = view;
+    GesturePane pane = new GesturePane(node);
+    parent.getChildren().add(pane);
+    // addButtons();
     Node start = qdb.retrieveNode(100);
     Node target = qdb.retrieveNode(130);
-    int x1 = start.getXCoord();
-    int y1 = start.getYCoord();
-    int x2 = target.getXCoord();
-    int y2 = target.getYCoord();
-    Line line = new Line();
-    line.setStartX(x1);
-    line.setStartY(y1);
-    line.setEndX(x2);
-    line.setEndY(y2);
-    line.setStyle("-fx-stroke: red;");
-    line.setStrokeWidth(3);
-    root.getChildren().add(line);
-    int x11 = 100;
-    int y11 = 100;
-    int x22 = 500;
-    int y22 = 500;
-    Line line2 = new Line();
-    line.setStartX(x11);
-    line.setStartY(y11);
-    line.setEndX(x22);
-    line.setEndY(y22);
-    line.setStyle("-fx-stroke: red;");
-    line.setStrokeWidth(3);
-    root.getChildren().add(line2);
-     */
-    Node start = qdb.retrieveNode(2315);
-    Node target = qdb.retrieveNode(2240);
     drawLines(start, target);
-    //    int x1 = 0;
-    //    int y1 = 0;
-    //    int x2 = 100;
-    //    int y2 = 100;
-    //    Line line = new Line(x1, y1, x2, y2);
-    //    line.setStyle("-fx-stroke: red;");
-    //    line.setStrokeWidth(3);
-    //    parent.getChildren().add(line);
+    pane.setOnMouseClicked(
+        e -> {
+          if (e.getButton() == MouseButton.PRIMARY && e.getClickCount() == 2) {
+            Point2D pivotOnTarget =
+                pane.targetPointAt(new Point2D(e.getX(), e.getY()))
+                    .orElse(pane.targetPointAtViewportCentre());
+            pane.animate(Duration.millis(200))
+                .interpolateWith(Interpolator.EASE_BOTH)
+                .zoomBy(pane.getCurrentScale(), pivotOnTarget);
+          }
+        });
+  }
+
+  public void addButtons() {
+    List<Node> nodes = qdb.retrieveAllNodes();
+    for (Node n : nodes) {
+      int x = n.getXCoord() / 5;
+      int y = n.getYCoord() / 5;
+      Button node = new Button();
+      node.setLayoutX(x);
+      node.setLayoutY(y);
+      parent.getChildren().add(node);
+    }
   }
 
   public void addLine(Node n, Node next) {
@@ -81,7 +80,7 @@ public class PathfindingController {
   public void drawLines(Node start, Node target) throws IOException {
     List<Node> path = Star2.aStar(start, target);
     if (path.size() > 0) {
-      for (int i = path.size() - 1; i >= 1; i--) {
+      for (int i = path.size() - 1; i >= 1; i--) { // change to i++ when calling the exist one
         Node n = path.get(i);
         Node next = path.get(i - 1);
         addLine(n, next);
