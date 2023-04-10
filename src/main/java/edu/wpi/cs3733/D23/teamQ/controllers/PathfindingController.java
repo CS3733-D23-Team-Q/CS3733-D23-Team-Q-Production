c9020f0d9100b15f90463ca5a914f3faad1c6489
Kelu Liu <keluliu5@gmail.com>
2023-04-10 03:20:07 -0400

    grouped elements 'on' imageview

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
@@ -5,10 +5,12 @@ import edu.wpi.cs3733.D23.teamQ.Pathfinding.Star2;
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javafx.animation.Interpolator;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
@@ -21,20 +23,21 @@ public class PathfindingController {
   Qdb qdb = Qdb.getInstance();
   // Stage stage = App.getPrimaryStage();
   Alert alert = new Alert();
-  @FXML Pane parent;
-  /*
-  @FXML HBox parent;
+  @FXML Pane root;
+  @FXML Group parent;
   @FXML ImageView map;
-   */
 
   @FXML
   public void initialize() throws IOException {
-    ImageView view = new ImageView(getClass().getResource("/L1.png").toExternalForm());
-    view.setFitWidth(1000);
-    view.setFitHeight(680);
-    javafx.scene.Node node = view;
+    // ImageView view = new ImageView(getClass().getResource("/L1.png").toExternalForm());
+    // view.setFitWidth(1000);
+    // view.setFitHeight(680);
+
+    addButtons();
+
+    javafx.scene.Node node = parent;
     GesturePane pane = new GesturePane(node);
-    parent.getChildren().add(pane);
+    root.getChildren().add(pane);
     // addButtons();
     Node start = qdb.retrieveNode(100);
     Node target = qdb.retrieveNode(130);
@@ -54,12 +57,25 @@ public class PathfindingController {
 
   public void addButtons() {
     List<Node> nodes = qdb.retrieveAllNodes();
+    List<Node> L1nodes = new ArrayList<>();
     for (Node n : nodes) {
+      if (n.getFloor().equals("L1")) {
+        L1nodes.add(n);
+      }
+    }
+    for (Node n : L1nodes) {
       int x = n.getXCoord() / 5;
       int y = n.getYCoord() / 5;
       Button node = new Button();
       node.setLayoutX(x);
       node.setLayoutY(y);
+      node.setStyle(
+          "-fx-background-radius: 5em;"
+              + "-fx-min-width: 3px;"
+              + "-fx-min-height: 3px;"
+              + "-fx-max-width: 3px;"
+              + "-fx-max-height: 3px;"
+              + "-fx-background-insets: 0px;");
       parent.getChildren().add(node);
     }
   }
@@ -70,7 +86,7 @@ public class PathfindingController {
     int x2 = next.getXCoord() / 5;
     int y2 = next.getYCoord() / 5;
     Line line = new Line(x1, y1, x2, y2);
-    line.setStyle("-fx-stroke: red;");
+    line.setStyle("-fx-stroke: blue;");
     line.setStrokeWidth(3);
     // line.toFront();
     parent.getChildren().add(line);
