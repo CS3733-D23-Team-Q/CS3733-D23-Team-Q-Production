3ba7c3bc9086bb6d4616064a8c33755e6bbb8c64
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-09 22:15:30 -0400

    working menu

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MenuRootController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MenuRootController.java
@@ -1,21 +1,52 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
+import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
+import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 public class MenuRootController {
-    @FXML
-    MFXButton home;
-    @FXML
-    MFXButton people;
-    @FXML
-    MFXButton navigation;
-    @FXML
-    MFXButton servicerequests;
-    @FXML
-    MFXButton statistics;
-    @FXML
-    MFXButton settings;
-    @FXML
-    MFXButton signout;
+  @FXML MFXButton home;
+  @FXML MFXButton people;
+  @FXML MFXButton navigation;
+  @FXML MFXButton servicerequests;
+  @FXML MFXButton statistics;
+  @FXML MFXButton settings;
+  @FXML MFXButton signout;
+
+  @FXML
+  public void homeClicked() {
+    Navigation.navigate(Screen.HOME);
+  }
+
+  @FXML
+  public void peopleClicked() {
+    Navigation.navigate(Screen.PROFILE_PAGE);
+  }
+
+  @FXML
+  public void navigationClicked() {
+    Navigation.navigate(Screen.PATH_TEXT);
+  }
+
+  @FXML
+  public void srClicked() {
+    Navigation.navigate(Screen.SERVICE_REQUEST_HUB);
+  }
+
+  @FXML
+  public void statisticsClicked() {
+    Platform.exit();
+  }
+
+  @FXML
+  public void settingsClicked() {
+    Navigation.navigate(Screen.HELP);
+  }
+
+  @FXML
+  public void signoutClicked() {
+    Navigation.logout();
+  }
 }
--- src/main/java/edu/wpi/cs3733/D23/teamQ/navigation/Screen.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/navigation/Screen.java
@@ -25,7 +25,8 @@ public enum Screen {
   LIST_REQUESTS("views/ListServiceRequests.fxml", "List of Service Requests"),
   PATIENT_TRANSPORT_REQUEST("views/PatientTransportRequest.fxml", "Patient Transport Request"),
   EDIT_PROFILE("views/EditProfile.fxml", "Edit Profile Page"),
-  PROFILE_PAGE("views/ProfilePage.fxml", "Profile Page");
+  PROFILE_PAGE("views/ProfilePage.fxml", "Profile Page"),
+  MENU_PANE("views/MenuRoot.fxml", "Menu Pane");
 
   private final String filename;
   private final String title;
