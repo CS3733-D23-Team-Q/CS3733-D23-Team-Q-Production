4aeee2e9b8e0c3700383cf3c4121c3e4d0aaa34f
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-10 14:57:21 -0400

    set up all submenus

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MenuRootController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MenuRootController.java
@@ -18,40 +18,54 @@ public class MenuRootController {
 
   @FXML
   public void initialize() {
+    home.setStyle("-fx-background-color: #012d5a");
+    people.setStyle("-fx-background-color: #012d5a");
+    navigation.setStyle("-fx-background-color: #012d5a");
+    servicerequests.setStyle("-fx-background-color: #012d5a");
+    statistics.setStyle("-fx-background-color: #012d5a");
+    settings.setStyle("-fx-background-color: #012d5a");
+    signout.setStyle("-fx-background-color: #012d5a");
   }
 
   @FXML
   public void homeClicked() {
+    home.setStyle("-fx-background-color: #f1f1f1");
     Navigation.navigate(Screen.HOME);
   }
 
   @FXML
   public void peopleClicked() {
+    people.setStyle("-fx-background-color: #f1f1f1");
     Navigation.navigate(Screen.PROFILE_PAGE);
   }
 
   @FXML
   public void navigationClicked() {
+    navigation.setStyle("-fx-background-color: #f1f1f1");
     Navigation.navigate(Screen.PATH_TEXT);
   }
 
   @FXML
   public void srClicked() {
+    servicerequests.setStyle("-fx-background-color: #f1f1f1");
     Navigation.navigate(Screen.SERVICE_REQUEST_HUB);
   }
 
   @FXML
   public void statisticsClicked() {
+    statistics.setStyle("-fx-background-color: #f1f1f1");
     Platform.exit();
   }
 
   @FXML
   public void settingsClicked() {
+    settings.setStyle("-fx-background-color: #f1f1f1");
     Navigation.navigate(Screen.HELP);
   }
 
   @FXML
   public void signoutClicked() {
+    signout.setStyle("-fx-background-color: #f1f1f1");
     Navigation.logout();
   }
 }
