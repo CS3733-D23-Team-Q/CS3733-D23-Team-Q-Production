91aa22cd306a1531111036855b3352fd24f3386d
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-09 20:58:54 -0400

    started menu setup

---
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MenuRootController.java
@@ -0,0 +1,21 @@
+package edu.wpi.cs3733.D23.teamQ.controllers;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class MenuRootController {
+    @FXML
+    MFXButton home;
+    @FXML
+    MFXButton people;
+    @FXML
+    MFXButton navigation;
+    @FXML
+    MFXButton servicerequests;
+    @FXML
+    MFXButton statistics;
+    @FXML
+    MFXButton settings;
+    @FXML
+    MFXButton signout;
+}
