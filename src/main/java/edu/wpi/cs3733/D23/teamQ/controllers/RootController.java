895c83a81b32b1b3a807af07a51394955003ced4
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-11 17:48:33 -0400

    rootRight exists

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/RootController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/RootController.java
@@ -4,7 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class RootController {
-  public AnchorPane rootAnchor;
+  @FXML public AnchorPane rootCenter;
+  @FXML public AnchorPane rootRight;
   @FXML MenuRootController menuController;
 
   @FXML
