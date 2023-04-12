cb190b0ba6b4a875d6226add6907b7e1204a567f
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2023-04-11 23:02:25 -0400

    round 2

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/OfficeSuppliesRequestController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/OfficeSuppliesRequestController.java
@@ -7,14 +7,13 @@ import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.sql.Date;
+import java.time.LocalDate;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-
-import java.sql.Date;
-import java.time.LocalDate;
 
 public class OfficeSuppliesRequestController {
   @FXML ChoiceBox assigneeField;
@@ -40,12 +39,13 @@ public class OfficeSuppliesRequestController {
     assigneeField.setValue(ListServiceRequestController.getOfficeRequest().getAssignee());
     roomNumberField.setValue(ListServiceRequestController.getOfficeRequest().getNode());
     dateField.setValue(
-            LocalDate.of(
-                    ListServiceRequestController.getOfficeRequest().getDate().getYear(),
-                    ListServiceRequestController.getOfficeRequest().getDate().getMonth(),
-                    ListServiceRequestController.getOfficeRequest().getDate().getDay()));
+        LocalDate.of(
+            ListServiceRequestController.getOfficeRequest().getDate().getYear(),
+            ListServiceRequestController.getOfficeRequest().getDate().getMonth(),
+            ListServiceRequestController.getOfficeRequest().getDate().getDay()));
     timeField.setText(ListServiceRequestController.getOfficeRequest().getTime());
-    specialInstructionsField.setText(ListServiceRequestController.getOfficeRequest().getSpecialInstructions());
+    specialInstructionsField.setText(
+        ListServiceRequestController.getOfficeRequest().getSpecialInstructions());
     itemRequestedField.setValue(ListServiceRequestController.getOfficeRequest().getItem());
     quantityField.setText("" + ListServiceRequestController.getOfficeRequest().getQuantity());
   }
@@ -59,28 +59,25 @@ public class OfficeSuppliesRequestController {
   @FXML
   public void submitButtonClicked() {
 
-        Qdb qdb = Qdb.getInstance();
-        if (((String) quantityField.getText()).equals("Select Quantity")) {
-          quantityField.setText("0");
-        }
+    Qdb qdb = Qdb.getInstance();
+    if (((String) quantityField.getText()).equals("Select Quantity")) {
+      quantityField.setText("0");
+    }
 
-        OfficeSuppliesRequest newOSR =
-            new OfficeSuppliesRequest(
-                ListServiceRequestController.getOfficeRequest().getRequestID(),
-                "temp user",
-                0,
-                    (String) assigneeField.getValue(),
-                    (Node)roomNumberField.getValue(),
-                specialInstructionsField.getText(),
-                Date.valueOf(dateField.getValue()),
-                timeField.getText(),
-                (String) itemRequestedField.getValue(),
-                Integer.parseInt((String) quantityField.getText()));
+    OfficeSuppliesRequest newOSR =
+        new OfficeSuppliesRequest(
+            ListServiceRequestController.getOfficeRequest().getRequestID(),
+            "temp user",
+            0,
+            (String) assigneeField.getValue(),
+            (Node) roomNumberField.getValue(),
+            specialInstructionsField.getText(),
+            Date.valueOf(dateField.getValue()),
+            timeField.getText(),
+            (String) itemRequestedField.getValue(),
+            Integer.parseInt((String) quantityField.getText()));
 
-
-        // qdb.addFlowerRequest(newOSR);
-        Navigation.navigate(Screen.HOME);
-
-
+     qdb.addOfficeSuppliesRequest(newOSR);
+    Navigation.navigate(Screen.HOME);
   }
 }
--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/OfficeSuppliesRequestDisplayController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/OfficeSuppliesRequestDisplayController.java
@@ -1,7 +1,6 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
-import edu.wpi.cs3733.D23.teamQ.db.obj.FlowerRequest;
 import edu.wpi.cs3733.D23.teamQ.db.obj.OfficeSuppliesRequest;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
@@ -17,67 +16,70 @@ import javafx.scene.control.ChoiceBox;
 
 public class OfficeSuppliesRequestDisplayController {
 
-    ObservableList<String> officeItemList =
-            FXCollections.observableArrayList("Printer Paper (by ream)", "Pencil", "Pen", "Highlighter", "Notepad");
+  ObservableList<String> officeItemList =
+      FXCollections.observableArrayList(
+          "Printer Paper (by ream)", "Pencil", "Pen", "Highlighter", "Notepad");
 
-    @FXML MFXButton deleteButton;
-    @FXML MFXButton backButton;
-    @FXML MFXButton updateButton;
+  @FXML MFXButton deleteButton;
+  @FXML MFXButton backButton;
+  @FXML MFXButton updateButton;
 
-    @FXML ChoiceBox assigneeField;
-    @FXML ChoiceBox roomNumberField;
-    @FXML MFXDatePicker dateField;
-    @FXML MFXTextField timeField;
-    @FXML ChoiceBox itemRequestedField;
-    @FXML MFXTextField quantityField;
-    @FXML MFXTextField specialInstructionsField;
+  @FXML ChoiceBox assigneeField;
+  @FXML ChoiceBox roomNumberField;
+  @FXML MFXDatePicker dateField;
+  @FXML MFXTextField timeField;
+  @FXML ChoiceBox itemRequestedField;
+  @FXML MFXTextField quantityField;
+  @FXML MFXTextField specialInstructionsField;
 
-    @FXML
-    public void initialize() {
-        itemRequestedField.setItems(officeItemList);
+  @FXML
+  public void initialize() {
+    itemRequestedField.setItems(officeItemList);
 
-        assigneeField.setValue(ListServiceRequestController.getOfficeRequest().getAssignee());
-        roomNumberField.setValue(ListServiceRequestController.getOfficeRequest().getNode());
-        dateField.setValue(
-                LocalDate.of(
-                        ListServiceRequestController.getConferenceRequest().getDate().getYear(),
-                        ListServiceRequestController.getConferenceRequest().getDate().getMonth(),
-                        ListServiceRequestController.getConferenceRequest().getDate().getDay()));
-        timeField.setText(ListServiceRequestController.getConferenceRequest().getTime());
-        specialInstructionsField.setText(
-                ListServiceRequestController.getConferenceRequest().getSpecialInstructions());
-        quantityField.setText(String.valueOf(ListServiceRequestController.getOfficeRequest().getQuantity()));
-    }
+    assigneeField.setValue(ListServiceRequestController.getOfficeRequest().getAssignee());
+    roomNumberField.setValue(ListServiceRequestController.getOfficeRequest().getNode());
+    dateField.setValue(
+        LocalDate.of(
+            ListServiceRequestController.getConferenceRequest().getDate().getYear(),
+            ListServiceRequestController.getConferenceRequest().getDate().getMonth(),
+            ListServiceRequestController.getConferenceRequest().getDate().getDay()));
+    timeField.setText(ListServiceRequestController.getConferenceRequest().getTime());
+    specialInstructionsField.setText(
+        ListServiceRequestController.getConferenceRequest().getSpecialInstructions());
+    quantityField.setText(
+        String.valueOf(ListServiceRequestController.getOfficeRequest().getQuantity()));
+  }
 
-    @FXML
-    public void deleteButtonClicked() {
-        Qdb qdb = Qdb.getInstance();
-        qdb.deleteOfficeSuppliesRequest(ListServiceRequestController.getOfficeRequest().getRequestID());
-    }
+  @FXML
+  public void deleteButtonClicked() {
+    Qdb qdb = Qdb.getInstance();
+    qdb.deleteOfficeSuppliesRequest(ListServiceRequestController.getOfficeRequest().getRequestID());
+  }
 
-    @FXML
-    public void backButtonClicked() {
-        Navigation.navigate(Screen.HOME);
-    }
+  @FXML
+  public void backButtonClicked() {
+    Navigation.navigate(Screen.HOME);
+  }
 
-    @FXML
-    public void updateButtonClicked() {
+  @FXML
+  public void updateButtonClicked() {
 
-        Qdb qdb = Qdb.getInstance();
+    Qdb qdb = Qdb.getInstance();
 
-        OfficeSuppliesRequest newOR =
-                new OfficeSuppliesRequest(
-                        ListServiceRequestController.getOfficeRequest().getRequestID(),
-                        "temp requester",
-                        0,
-                        "temp assignee",
-                        ListServiceRequestController.getOfficeRequest().getNode(),
-                        (String) specialInstructionsField.getText(),
-                        Date.valueOf(dateField.getValue()),
-                        timeField.getText(),
-                        (String) itemRequestedField.getValue(),
-                        Integer.parseInt((String) quantityField.getText()));
+    OfficeSuppliesRequest newOR =
+        new OfficeSuppliesRequest(
+            ListServiceRequestController.getOfficeRequest().getRequestID(),
+            "temp requester",
+            0,
+            "temp assignee",
+            ListServiceRequestController.getOfficeRequest().getNode(),
+            (String) specialInstructionsField.getText(),
+            Date.valueOf(dateField.getValue()),
+            timeField.getText(),
+            (String) itemRequestedField.getValue(),
+            Integer.parseInt((String) quantityField.getText()));
 
-        qdb.updateOfficeSuppliesRequest(ListServiceRequestController.getFlowerRequest().getRequestID(), newOR);
-    }
+    qdb.updateOfficeSuppliesRequest(
+        ListServiceRequestController.getFlowerRequest().getRequestID(), newOR);
+  }
 }
