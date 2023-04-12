e7e2775eb3faa4beed88601133757d315ed7e63a
cam40419 <83707970+cam40419@users.noreply.github.com>
2023-04-11 21:49:29 -0400

    Started conference request submit

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ConferenceRoomRequestController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ConferenceRoomRequestController.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
+import edu.wpi.cs3733.D23.teamQ.db.obj.ConferenceRequest;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
@@ -12,67 +13,73 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
+import java.sql.Date;
+
 public class ConferenceRoomRequestController {
 
-  @FXML ChoiceBox assigneeField;
-  @FXML ChoiceBox roomNumberField;
-  @FXML MFXDatePicker dateField;
-  @FXML MFXTextField timeField;
-  @FXML ChoiceBox foodField;
-  @FXML MFXTextField specialInstructionsField;
-  ObservableList<String> foodOptionsList =
-      FXCollections.observableArrayList(
-          "Brunch spread", "Dinner spread", "Snack spread", "No food");
-  @FXML Button resetButton;
-  @FXML Button cancelButton;
-  @FXML Button submitButton;
+    @FXML
+    ChoiceBox assigneeField;
+    @FXML
+    ChoiceBox roomNumberField;
+    @FXML
+    MFXDatePicker dateField;
+    @FXML
+    MFXTextField timeField;
+    @FXML
+    ChoiceBox foodField;
+    @FXML
+    MFXTextField specialInstructionsField;
+    ObservableList<String> foodOptionsList = FXCollections.observableArrayList("Brunch spread", "Dinner spread", "Snack spread", "No food");
+    @FXML
+    Button resetButton;
+    @FXML
+    Button cancelButton;
+    @FXML
+    Button submitButton;
 
-  @FXML
-  public void initialize() {
-    this.foodField.setValue("Select Food Option");
-    this.foodField.setItems(foodOptionsList);
-  }
+    @FXML
+    public void initialize() {
+        this.foodField.setValue("Select Food Option");
+        this.foodField.setItems(foodOptionsList);
+    }
 
-  @FXML
-  public void resetButtonClicked() {}
+    @FXML
+    public void resetButtonClicked() {
+    }
 
-  @FXML
-  public void cancelButtonClicked() {
-    Navigation.navigate(Screen.SERVICE_PLACEHOLDER);
-  }
+    @FXML
+    public void cancelButtonClicked() {
+        Navigation.navigate(Screen.SERVICE_PLACEHOLDER);
+    }
 
-  @FXML
-  public void submitButtonClicked() {
-    Qdb qdb = Qdb.getInstance();
+    @FXML
+    public void submitButtonClicked() {
+        Qdb qdb = Qdb.getInstance();
 
-    //    ConferenceRequest newCCR =
-    //        new ConferenceRequest(
-    //            0,
-    //            "temp user",
-    //            0,
-    //            assigneeField.getText(),
-    //            qdb.retrieveNode(Integer.parseInt(roomNumberField.getText())),
-    //            specialInstructionsField.getText(),
-    //            // add for date
-    //            // add for time
-    //            (String) foodField.getValue());
-    //    qdb.addConferenceRequest(newCCR);
+        ConferenceRequest cr = new ConferenceRequest(
+                LoginController.getLoginUsername(),
+                0,
+                (String)assigneeField.getValue(),
+                qdb.retrieveNode(Integer.parseInt((String)roomNumberField.getValue())),
+                specialInstructionsField.getText(),
+                Date.valueOf(dateField.getCurrentDate()),
+                timeField.getText(),
+                (String)foodField.getValue());
+        Navigation.navigate(Screen.HOME);
+    }
 
-    Navigation.navigate(Screen.HOME);
-  }
+    @FXML
+    public void homeItemClicked() {
+        Navigation.navigate(Screen.HOME);
+    }
 
-  @FXML
-  public void homeItemClicked() {
-    Navigation.navigate(Screen.HOME);
-  }
+    @FXML
+    public void exitItemClicked() {
+        Platform.exit();
+    }
 
-  @FXML
-  public void exitItemClicked() {
-    Platform.exit();
-  }
-
-  @FXML
-  public void profileItemClicked() {
-    Navigation.navigate(Screen.PROFILE_PAGE);
-  }
+    @FXML
+    public void profileItemClicked() {
+        Navigation.navigate(Screen.PROFILE_PAGE);
+    }
 }
