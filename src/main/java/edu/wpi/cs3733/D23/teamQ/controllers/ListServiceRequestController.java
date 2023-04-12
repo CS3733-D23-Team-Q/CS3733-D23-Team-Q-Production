a62c4a872834afa610206ded107ee92ea04152ad
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-11 22:13:25 -0400

    furniture request display controller

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
@@ -1,8 +1,10 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
+
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.ConferenceRequest;
 import edu.wpi.cs3733.D23.teamQ.db.obj.FlowerRequest;
+import edu.wpi.cs3733.D23.teamQ.db.obj.FurnitureRequest;
 import edu.wpi.cs3733.D23.teamQ.db.obj.ServiceRequest;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
@@ -12,18 +14,24 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+
 public class ListServiceRequestController {
   @FXML TableView<ServiceRequest> tableView;
   @FXML TableColumn<ServiceRequest, Integer> requestID;
   @FXML TableColumn<ServiceRequest, Integer> progress;
   @FXML TableColumn<ServiceRequest, String> roomNumber;
 
+
   private static FlowerRequest flowerRequest;
   private static ConferenceRequest conferenceRequest;
+  private static FurnitureRequest furnitureRequest;
+
 
   Qdb qdb = Qdb.getInstance();
 
+
   public ListServiceRequestController() {}
+
 
   @FXML
   public void initialize() {
@@ -33,30 +41,37 @@ public class ListServiceRequestController {
     tableView.setItems((ObservableList<ServiceRequest>) qdb.retrieveAllServiceRequests());
   }
 
+
   @FXML
   public void rowSelected() {
     if(qdb.retrieveFlowerRequest(
-      tableView.getSelectionModel().getSelectedItems().get(0).getRequestID())
-       != null) {
-       flowerRequest =
-        qdb.retrieveFlowerRequest(
-          tableView.getSelectionModel().getSelectedItems().get(0).getRequestID());
+            tableView.getSelectionModel().getSelectedItems().get(0).getRequestID())
+            != null) {
+      flowerRequest =
+              qdb.retrieveFlowerRequest(
+                      tableView.getSelectionModel().getSelectedItems().get(0).getRequestID());
       Navigation.navigate(Screen.FLOWER_REQUEST_DISPLAY);
     } else if (qdb.retrieveConferenceRequest(
             tableView.getSelectionModel().getSelectedItems().get(0).getRequestID())
-        != null) {
+            != null) {
       conferenceRequest =
-          qdb.retrieveConferenceRequest(
-              tableView.getSelectionModel().getSelectedItems().get(0).getRequestID());
+              qdb.retrieveConferenceRequest(
+                      tableView.getSelectionModel().getSelectedItems().get(0).getRequestID());
       Navigation.navigate(Screen.CONFERENCE_ROOM_REQUEST_DISPLAY);
     }
   }
+
 
   public static ConferenceRequest getConferenceRequest() {
     return conferenceRequest;
   }
 
+
   public static FlowerRequest getFlowerRequest() {
     return flowerRequest;
   }
+
+
+  public static FurnitureRequest getFurnitureRequest(){return furnitureRequest;}
 }
+
