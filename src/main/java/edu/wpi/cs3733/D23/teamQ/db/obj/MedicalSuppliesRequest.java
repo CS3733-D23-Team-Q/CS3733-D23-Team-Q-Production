5d59231903d9c1c5140ea9f01cfbd7daa5858d87
cam40419 <83707970+cam40419@users.noreply.github.com>
2023-04-17 18:50:44 -0400

    Revert "Added requestType to all service requests"
    This reverts commit f237fd381192cd9fa6bb9a97bed6601f8d79011c.

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/MedicalSuppliesRequest.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/MedicalSuppliesRequest.java
@@ -1,8 +1,6 @@
 package edu.wpi.cs3733.D23.teamQ.db.obj;
 
 import edu.wpi.cs3733.D23.teamQ.db.dao.IServiceRequest;
-
-import java.lang.reflect.Type;
 import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +10,6 @@ import lombok.Setter;
 public class MedicalSuppliesRequest extends ServiceRequest implements IServiceRequest {
   private String item;
   private int quantity;
-  private Type requestType = this.getClass();
 
   public MedicalSuppliesRequest(
       int requestID,
