be763bc984d2b86d81112549eafee27bf5ace82b
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-18 19:09:47 -0400

    BFS controller working - bfs works

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathfindingController.java
@@ -1,10 +1,7 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
 import edu.wpi.cs3733.D23.teamQ.Alert;
-import edu.wpi.cs3733.D23.teamQ.Pathfinding.AStar;
-import edu.wpi.cs3733.D23.teamQ.Pathfinding.Context;
-import edu.wpi.cs3733.D23.teamQ.Pathfinding.DFS;
-import edu.wpi.cs3733.D23.teamQ.Pathfinding.Djikstra;
+import edu.wpi.cs3733.D23.teamQ.Pathfinding.*;
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Location;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Move;
@@ -37,6 +34,7 @@ public class PathfindingController {
   Qdb qdb = Qdb.getInstance();
   // Stage stage = App.getPrimaryStage();
   Context pathfindingAlgorithmSelection = new Context();
+  BFS bfs = new BFS();
   AStar aStar = new AStar();
   DFS dfs = new DFS();
   Djikstra djikstra = new Djikstra();
@@ -401,8 +399,8 @@ public class PathfindingController {
       // that changes whenever the button is clicked)
       path = pathfindingAlgorithmSelection.run(start, target);
     } else if (algorithm.equals("bfs")) {
-      // pathfindingAlgorithmSelection.setPathfindingAlgorithm(bfs);
-      // path = pathfindingAlgorithmSelection.run(start, target);
+      pathfindingAlgorithmSelection.setPathfindingAlgorithm(bfs);
+      path = pathfindingAlgorithmSelection.run(start, target);
     } else if (algorithm.equals("dfs")) {
       pathfindingAlgorithmSelection.setPathfindingAlgorithm(dfs);
       path = pathfindingAlgorithmSelection.run(start, target);
