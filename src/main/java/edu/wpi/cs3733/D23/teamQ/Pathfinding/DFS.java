abebea5797f4535f9685c524c6d5e550ef7dee93
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-18 18:26:47 -0400

    Revert "Graphical algorithms merged 2"

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/DFS.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/DFS.java
@@ -9,75 +9,37 @@ public class DFS implements IPathfinding {
     ArrayList<Node> visitedList = new ArrayList<Node>(); // list of visited nodes
     ArrayList<Node> openList = new ArrayList<Node>();
     ArrayList<Node> path = new ArrayList<Node>(); // path list if I need it
-    String floor = start.getFloor();
     openList.add(start);
-    visitedList.add(start);
-    // path.add(start);
+    path.add(start);
     while (!openList.isEmpty()) {
-      boolean nextChosen = false;
       Node current = openList.get(0);
       // in case the end node is on a diff floor
+
       if (current.equals(end)) {
-        path.add(current);
         return path;
       }
       if (current.getEdges().size() == 0) {
         System.out.println("This node is a dead end " + current);
       }
-      ArrayList<Node> nodesAvailable = new ArrayList<Node>();
-      for (Edge edgePath : current.getEdges()) {
-        nodesAvailable.add(edgePath.getStartNode());
-        nodesAvailable.add(edgePath.getEndNode());
-      }
-      if (!visitedList.contains(current.getEdges().get(0).getEndNode())
-          && !current.getEdges().isEmpty()
-          && !current.equals(current.getEdges().get(0).getEndNode())
-          && floor.equalsIgnoreCase(current.getEdges().get(0).getEndNode().getFloor())) {
+      if (!visitedList.contains(current.getEdges().get(0).getEndNode())) {
         openList.add(current.getEdges().get(0).getEndNode());
-        openList.remove(current);
         path.add(current);
         visitedList.add(current);
-        nextChosen = true;
-      } else if (!visitedList.contains(current.getEdges().get(0).getStartNode())
-          && !current.getEdges().isEmpty()
-          && !current.equals(current.getEdges().get(0).getStartNode())
-          && floor.equalsIgnoreCase(current.getEdges().get(0).getStartNode().getFloor())) {
+      } else if (!visitedList.contains(current.getEdges().get(0).getStartNode())) {
         openList.add(current.getEdges().get(0).getStartNode());
-        openList.remove(current);
         path.add(current);
         visitedList.add(current);
-        nextChosen = true;
       } else {
-        for (Node backup : nodesAvailable) {
-          if (!visitedList.contains(backup)
-              && !current.equals(backup)
-              && floor.equalsIgnoreCase(backup.getFloor())) {
-            openList.add(backup);
-            openList.remove(current);
+        // look through other edges until an unvisited node is found
+        for (Edge index : current.getEdges()) {
+          if (!visitedList.contains(index.getStartNode())) {
+            openList.add(index.getStartNode());
             path.add(current);
             visitedList.add(current);
-            nextChosen = true;
-          }
-        }
-      }
-      int i = 1;
-      while (!nextChosen) {
-        Node previous = path.get(path.size() - i);
-        ArrayList<Node> previousNodes = new ArrayList<Node>();
-        i++;
-        for (Edge previousEdge : previous.getEdges()) {
-          previousNodes.add(previousEdge.getStartNode());
-          previousNodes.add(previousEdge.getEndNode());
-        }
-        for (Node previousNode : previousNodes) {
-          if (!path.contains(previousNode)
-              && !visitedList.contains(previousNode)
-              && floor.equalsIgnoreCase(previousNode.getFloor())) {
-            openList.add(previousNode);
-            openList.remove(current);
+          } else if (!visitedList.contains(index.getEndNode())) {
+            openList.add(index.getEndNode());
             path.add(current);
             visitedList.add(current);
-            nextChosen = true;
           }
         }
       }
--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Djikstra.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Djikstra.java
@@ -9,71 +9,44 @@ public class Djikstra implements IPathfinding {
     ArrayList<Node> closedList = new ArrayList<Node>();
     ArrayList<Node> openList = new ArrayList<Node>();
     ArrayList<Node> path = new ArrayList<Node>();
+
     openList.add(start);
-    closedList.add(start);
     while (!openList.isEmpty()) {
       Node current = openList.get(0);
-      if (current == null) {
-        return path;
-      }
       if (current.equals(end)) {
-        path.add(current);
         return path;
       }
       // write in elevator specifications
+
       double lowestLocalCost = 10000000.0;
       Node chosen = null;
       for (Edge thisOne : current.getEdges()) {
         if (!closedList.contains(thisOne.getStartNode())
             && !thisOne.getStartNode().equals(current)
-            && !path.contains(thisOne.getStartNode())
-            && thisOne.getStartNode().getFloor().equalsIgnoreCase(start.getFloor())) {
-          double xDist = Math.abs(end.getXCoord() - thisOne.getStartNode().getXCoord());
-          double yDist = Math.abs(end.getYCoord() - thisOne.getStartNode().getYCoord());
+            && !path.contains(thisOne.getStartNode())) {
+          double xDist = Math.abs(thisOne.getStartNode().getXCoord() - current.getXCoord());
+          double yDist = Math.abs(thisOne.getStartNode().getYCoord() - current.getYCoord());
           double weight = Math.sqrt(xDist * xDist + yDist * yDist);
           if (weight < lowestLocalCost) {
             lowestLocalCost = weight;
             chosen = thisOne.getStartNode();
-            System.out.println();
           }
         } else if (!closedList.contains(thisOne.getEndNode())
             && !thisOne.getEndNode().equals(current)
-            && !path.contains(thisOne.getEndNode())
-            && thisOne.getEndNode().getFloor().equalsIgnoreCase(start.getFloor())) {
-          double xDist = Math.abs(end.getXCoord() - thisOne.getEndNode().getXCoord());
-          double yDist = Math.abs(end.getYCoord() - thisOne.getEndNode().getYCoord());
+            && !path.contains(thisOne.getEndNode())) {
+          double xDist = Math.abs(thisOne.getEndNode().getXCoord() - current.getXCoord());
+          double yDist = Math.abs(thisOne.getEndNode().getYCoord() - current.getYCoord());
           double weight = Math.sqrt(xDist * xDist + yDist * yDist);
           if (weight < lowestLocalCost) {
             lowestLocalCost = weight;
-            chosen = thisOne.getEndNode();
-            System.out.println();
+            chosen = thisOne.getStartNode();
           }
         }
       }
       path.add(current);
-      if (chosen == null) {
-        ArrayList<Node> altNodes = new ArrayList<Node>();
-        for (Node node : closedList) {
-          for (Edge edge : node.getEdges()) {
-            altNodes.add(edge.getStartNode());
-            altNodes.add(edge.getEndNode());
-          }
-        }
-        for (Node node2 : altNodes) {
-          if (!closedList.contains(node2)
-              && node2.getFloor().equalsIgnoreCase(start.getFloor())) { // "ELEV", STAI
-            chosen = node2;
-          }
-        }
-      }
-      if (chosen == null) {
-        System.out.println("COUNTERMEAUSRES LULW");
-      }
       openList.add(chosen);
       openList.remove(current);
       closedList.add(current);
-      System.out.println();
-      System.out.println("Added node " + current.getNodeID() + " to closedList");
     }
     return path;
   }
