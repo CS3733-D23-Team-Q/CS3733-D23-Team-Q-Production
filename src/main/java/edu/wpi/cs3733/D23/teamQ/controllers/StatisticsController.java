5fd0cbc74b24f6907b3be5705a261d4d841c109f
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-18 10:32:39 -0400

    stat page

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/StatisticsController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/StatisticsController.java
@@ -1,14 +1,18 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 
 public class StatisticsController {
   Qdb qdb = Qdb.getInstance();
   @FXML Label users;
   @FXML Label nodes;
+  @FXML PieChart nodePie;
   @FXML Label requests;
+
 
   @FXML
   public void initialize() {
