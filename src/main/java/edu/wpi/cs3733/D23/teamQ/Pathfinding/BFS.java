15dc0b71976cfdb7d5951576f3c7ff73c0e8e6fe
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-18 20:33:10 -0400

    idk format or something

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/BFS.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/BFS.java
@@ -8,8 +8,8 @@ public class BFS implements IPathfinding {
 
   @Override
   public ArrayList<Node> run(Node start, Node target) {
-    Queue<Node> queue = new LinkedList<>();
     ArrayList<Node> visited = new ArrayList<>();
+    Queue<Node> queue = new LinkedList<>();
     Map<Node, Node> parentMap = new HashMap<>();
 
     String floor = start.getFloor();
@@ -24,8 +24,7 @@ public class BFS implements IPathfinding {
       for (Edge edge : n.getEdges()) {
         Node neighbor = edge.getEndNode();
         if (!visited.contains(neighbor)) {
-
-          //        if (!visited.contains(neighbor) && floor.equalsIgnoreCase(neighbor.getFloor())){
+          //        if (!visited.contains(neighbor) && floor.equalsIgnoreCase(neighbor.getFloor()))
           visited.add(neighbor);
           parentMap.put(neighbor, n);
           queue.add(neighbor);
