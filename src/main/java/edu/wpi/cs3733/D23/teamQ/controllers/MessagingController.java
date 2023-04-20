630d14f510336f928ad667d396d32fcd18773f7c
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2023-04-20 19:25:43 -0400

    messages!

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MessagingController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/MessagingController.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
+import static java.lang.System.currentTimeMillis;
+
 import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Account;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Message;
@@ -11,6 +13,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -18,91 +21,93 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-import javax.swing.text.html.ImageView;
-
-import static java.lang.System.currentTimeMillis;
-
 public class MessagingController {
-    Account reciever;
-    Color active = new Color(55, 172, 43,1);
-    Color inactive = new Color(206, 60, 73,0);
+  Account receiver;
+  @FXML ImageView sendButton;
+  @FXML VBox messageVbox;
+  @FXML MFXTextField messageField;
+  @FXML MFXScrollPane messageSP;
+  @FXML MFXFilterComboBox peopleSelector;
 
-    @FXML ImageView sendButton;
-    @FXML VBox messageVbox;
-    @FXML MFXTextField messageField;
-    @FXML MFXScrollPane messageSP;
-    @FXML MFXFilterComboBox peopleSelector;
+  @FXML ImageView profilePicture;
+  @FXML Circle activeIndicator;
 
-    @FXML ImageView profilePicture;
-    @FXML Circle activeIndicator;
+  @FXML
+  public void initialize() {
+    Qdb qdb = Qdb.getInstance();
 
-    @FXML public void initialize(){
-        messageVbox.heightProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+    peopleSelector.setValue("");
+    peopleSelector.setItems(qdb.getAllNames());
+
+    messageVbox
+        .heightProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 messageSP.setVvalue((double) newValue);
-            }
-        });
+              }
+            });
 
-        //add listener for people
-        //peopleSelector.addEventHandler();
+    // Add new message listener for messages coming in
 
-        //Add new message listener
+    // add listener for changing who the message is being sent to
+    // peopleSelector.addEventHandler();
+    // Set image
+    // receiver = qdb.retrieveAccount(peopleSelector.getText());
+    // if (receiver.isActive()) activeIndicator.setStyle("-fx-fill: #37AC2B");
+    // else activeIndicator.setStyle("-fx-fill: #CE3C49");
+  }
 
-        Qdb qdb = Qdb.getInstance();
-        peopleSelector.setItems(qdb.getAllNames());
-        reciever = qdb.retrieveAccount(peopleSelector.getText());
-        //Need to set image
-        if (reciever.isActive()) activeIndicator.setFill(active);
-        else activeIndicator.setFill(inactive);
+  @FXML
+  public void sendButtonClicked() {
+    Qdb qdb = Qdb.getInstance();
+    String message = messageField.getText();
+    if (!message.isEmpty()) {
+      Message newMessage =
+          new Message(
+              qdb.retrieveAccount(LoginController.getUsername()),
+              receiver,
+              message,
+              currentTimeMillis());
 
+      HBox hbox = new HBox();
+      hbox.setAlignment(Pos.CENTER_RIGHT);
+      hbox.setPadding(new Insets(5, 5, 5, 10));
+
+      Text text = new Text(message);
+      TextFlow textFlow = new TextFlow(text);
+      textFlow.setStyle("-fx-background-color: #0167B1");
+      textFlow.setStyle("-fx-background-radius: 20px");
+      textFlow.setPadding(new Insets(5, 10, 5, 10));
+      text.setStyle("-fx-font-family: Roboto");
+      text.setFill(Color.BLACK);
+      hbox.getChildren().add(text);
+      messageVbox.getChildren().add(hbox);
+
+      // qdb.addMessage(newMessage);
+      messageField.clear();
+
+      System.out.println(message);
     }
+  }
 
-    @FXML public void sendButtonClicked(){
-        Qdb qdb = Qdb.getInstance();
-        String message = messageField.getText();
-        if(!message.isEmpty()){
-            Message newMessage = new Message(qdb.retrieveAccount(
-                    LoginController.getUsername()),
-                    reciever ,
-                    message,
-                    currentTimeMillis());
+  public void messageReceived(Message messageReceived) {
+    String message = messageReceived.getMessage();
 
-            HBox hbox = new HBox();
-            hbox.setAlignment(Pos.CENTER_RIGHT);
-            hbox.setPadding(new Insets(5,5,5,10));
+    HBox hbox = new HBox();
+    hbox.setAlignment(Pos.CENTER_LEFT);
+    hbox.setPadding(new Insets(5, 10, 5, 5));
 
-            Text text = new Text(message);
-            TextFlow textFlow = new TextFlow(text);
-            textFlow.setStyle("-fx-color: #0167B1");
-            textFlow.setStyle("-fx-background-radius: 20px");
-            textFlow.setPadding(new Insets(5,10,5,10));
-            //text.setFont();
-            text.setFill(Color.WHITE);
-            hbox.getChildren().add(textFlow);
-            messageVbox.getChildren().add(hbox);
-
-            //qdb.addMessage(newMessage);
-            messageField.clear();
-        }
-
-    }
-    public void messageReceived(Message messageReceived){
-        String message = messageReceived.getMessage();
-
-        HBox hbox = new HBox();
-        hbox.setAlignment(Pos.CENTER_LEFT);
-        hbox.setPadding(new Insets(5,10,5,5));
-
-        Text text = new Text(message);
-        TextFlow textFlow = new TextFlow(text);
-        textFlow.setStyle("-fx-color: #B4B4B4");
-        textFlow.setStyle("-fx-background-radius: 20px");
-        textFlow.setPadding(new Insets(5,10,5,10));
-        //text.setFont();
-        text.setFill(Color.WHITE);
-        hbox.getChildren().add(textFlow);
-        messageVbox.getChildren().add(hbox);
-    }
-
+    Text text = new Text(message);
+    TextFlow textFlow = new TextFlow(text);
+    textFlow.setStyle("-fx-color: #B4B4B4");
+    textFlow.setStyle("-fx-background-radius: 20px");
+    textFlow.setPadding(new Insets(5, 10, 5, 10));
+    text.setStyle("-fx-font-family: Roboto");
+    text.setFill(Color.BLACK);
+    hbox.getChildren().add(textFlow);
+    messageVbox.getChildren().add(hbox);
+  }
 }
--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/Message.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/Message.java
@@ -1,48 +1,48 @@
 package edu.wpi.cs3733.D23.teamQ.db.obj;
 
 public class Message {
-    private Account sender;
-    private Account receiver;
-    private String message;
+  private Account sender;
+  private Account receiver;
+  private String message;
 
-    private long timeStamp;
+  private long timeStamp;
 
-    public Message(Account sender, Account receiver, String message, long timeStamp) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.message = message;
-        this.timeStamp = timeStamp;
-    }
+  public Message(Account sender, Account receiver, String message, long timeStamp) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.message = message;
+    this.timeStamp = timeStamp;
+  }
 
-    public Account getSender() {
-        return sender;
-    }
+  public Account getSender() {
+    return sender;
+  }
 
-    public void setSender(Account sender) {
-        this.sender = sender;
-    }
+  public void setSender(Account sender) {
+    this.sender = sender;
+  }
 
-    public Account getReceiver() {
-        return receiver;
-    }
+  public Account getReceiver() {
+    return receiver;
+  }
 
-    public void setReceiver(Account receiver) {
-        this.receiver = receiver;
-    }
+  public void setReceiver(Account receiver) {
+    this.receiver = receiver;
+  }
 
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
-    public long getTimeStamp() {
-        return timeStamp;
-    }
+  public long getTimeStamp() {
+    return timeStamp;
+  }
 
-    public void setTimeStamp(long timeStamp) {
-        this.timeStamp = timeStamp;
-    }
+  public void setTimeStamp(long timeStamp) {
+    this.timeStamp = timeStamp;
+  }
 }
