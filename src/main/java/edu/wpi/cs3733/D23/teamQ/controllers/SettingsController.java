149ce4d953cade991d1e6a0f9aed26bc05c852aa
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-21 19:05:03 -0400

    kinda done
    dropdowns aren't working and no functionality

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SettingsController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SettingsController.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
-import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -11,7 +10,7 @@ import javafx.fxml.FXML;
 
 public class SettingsController {
 
-  Qdb qdb = Qdb.getInstance();
+  //  Qdb qdb = Qdb.getInstance();
   @FXML MFXFilterComboBox fontField;
   @FXML MFXFilterComboBox voiceField;
   @FXML MFXFilterComboBox languageField;
@@ -27,17 +26,7 @@ public class SettingsController {
 
   @FXML MFXButton backButton;
 
-  /*
-      this.assigneeField.setValue("");
-    this.assigneeField.setItems(qdb.getAllNames());
-    this.roomNumberField.setValue("");
-    this.roomNumberField.setItems(qdb.getAllLongNames());
-    this.itemRequestedField.setValue("");
-    this.itemRequestedField.setItems(itemList);
-    this.timeField.setValue("");
-    this.timeField.setItems(timeList);
-
-  */ @FXML
+  @FXML
   public void initialize() {
     this.fontField.setValue("");
     this.fontField.setItems(fontList);
