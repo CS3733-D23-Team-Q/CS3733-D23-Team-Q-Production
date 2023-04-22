17bce160d7c52ee17f128eaf5f6c55f0d1ea92d4
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-22 14:58:53 -0400

    all buttons declared

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/CreditsController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/CreditsController.java
@@ -10,8 +10,63 @@ public class CreditsController {
   @FXML Label linkDisplay;
   @FXML MFXButton copy;
   @FXML MFXButton clear;
+
+  // libraries
   @FXML MFXButton gesturefx;
   @FXML MFXButton materialfx;
+
+  // icons
+  @FXML MFXButton alert;
+  @FXML MFXButton alertBox;
+  @FXML MFXButton blueLogo;
+  @FXML MFXButton chevronLeft;
+  @FXML MFXButton chevronRight;
+  @FXML MFXButton close;
+  @FXML MFXButton confirm;
+  @FXML MFXButton down;
+  @FXML MFXButton edge;
+  @FXML MFXButton exit;
+  @FXML MFXButton exitBlue;
+  @FXML MFXButton flower;
+  @FXML MFXButton handshake;
+  @FXML MFXButton home;
+  @FXML MFXButton homeBlue;
+  @FXML MFXButton leftArrow;
+  @FXML MFXButton loc;
+  @FXML MFXButton loginExit;
+  @FXML MFXButton logo;
+  @FXML MFXButton map;
+  @FXML MFXButton map2;
+  @FXML MFXButton mapBlue;
+  @FXML MFXButton mapEditor;
+  @FXML MFXButton menu;
+  @FXML MFXButton move;
+  @FXML MFXButton node;
+  @FXML MFXButton people;
+  @FXML MFXButton peopleBlue;
+  @FXML MFXButton profile;
+  @FXML MFXButton rightArrow;
+  @FXML MFXButton sr;
+  @FXML MFXButton srBlue;
+  @FXML MFXButton settings;
+  @FXML MFXButton settingsBlue;
+  @FXML MFXButton stat;
+  @FXML MFXButton statBlue;
+  @FXML MFXButton up;
+
+  // images
+  @FXML MFXButton firstFloor;
+  @FXML MFXButton groundFloor;
+  @FXML MFXButton exterior;
+  @FXML MFXButton imagemark;
+  @FXML MFXButton lowerLevel1;
+  @FXML MFXButton lowerLevel2;
+  @FXML MFXButton secondFloor;
+  @FXML MFXButton shapiro;
+  @FXML MFXButton textLogo;
+  @FXML MFXButton thirdFloor;
+
+  // software
   @FXML MFXButton figma;
   @FXML MFXButton github;
   @FXML MFXButton intellij;
@@ -37,6 +92,7 @@ public class CreditsController {
     cb.setContent(content);
   }
 
+  // libraries
   @FXML
   public void gesturefxClicked() {
     linkDisplay.setText("https://github.com/tom91136/GestureFX");
@@ -47,6 +103,244 @@ public class CreditsController {
     linkDisplay.setText("https://github.com/palexdev/MaterialFX");
   }
 
+  // icons
+  @FXML
+  public void alertClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void alertBoxClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void blueLogoClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void chevronLeftClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void chevronRightClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void closeClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void confirmClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void downClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void edgeClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void exitClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void exitBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void flowerClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void handshakeClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void homeClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void homeBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void leftArrowClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void locClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void loginExitClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void logoClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void mapClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void map2Clicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void mapBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void mapEditorClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void menuClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void moveClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void nodeClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void peopleClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void peopleBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void profileClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void rightArrowClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void srClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void srBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void settingsClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void settingsBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void statClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void statBlueClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void upClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  // images
+  @FXML
+  public void firstFloorClicked() {
+    linkDisplay.setText("BWH First Floor");
+  }
+
+  @FXML
+  public void groundFloorClicked() {
+    linkDisplay.setText("BWH Second Flor");
+  }
+
+  @FXML
+  public void exteriorClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void imagemarkClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void lowerLevel1Clicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void lowerLevel2Clicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void secondFloorClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void shapiroClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void textLogoClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  @FXML
+  public void thirdFloorClicked() {
+    linkDisplay.setText("https://github.com/palexdev/MaterialFX");
+  }
+
+  // software
   @FXML
   public void figmaClicked() {
     linkDisplay.setText("https://www.figma.com/");
