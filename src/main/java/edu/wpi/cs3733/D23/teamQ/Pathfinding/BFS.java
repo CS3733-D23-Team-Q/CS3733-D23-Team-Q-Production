051987ab332050fb55c44570366e1b3e5db69abd
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-23 16:26:09 -0400

    BFS fully fixed

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/BFS.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/BFS.java
@@ -18,12 +18,13 @@ public class BFS implements IPathfinding {
 
     while (!queue.isEmpty()) {
       Node n = queue.poll();
+      boolean nodeChosen = false;
       if (n == target) {
         return visited;
       }
       if (n.getLocation().getNodeType().equalsIgnoreCase("ELEV")
-          && !n.getFloor()
-              .equalsIgnoreCase(endFloor)) { // branch to send to correct floor, if needed
+          && !n.getFloor().equalsIgnoreCase(endFloor)
+          && !nodeChosen) { // branch to send to correct floor, if needed
         ArrayList<Node> elevatorNodes = new ArrayList<Node>();
         for (Edge edge : n.getEdges()) {
           elevatorNodes.add(edge.getStartNode());
@@ -34,12 +35,14 @@ public class BFS implements IPathfinding {
             visited.add(node);
             parentMap.put(node, n);
             queue.add(node);
+            nodeChosen = true;
+            break;
           }
         }
       }
       if (n.getLocation().getNodeType().equalsIgnoreCase("ELEV")
-          && n.getFloor()
-              .equalsIgnoreCase(endFloor)) { // branch to send to correct floor, if needed
+          && n.getFloor().equalsIgnoreCase(endFloor)
+          && !nodeChosen) { // branch to send to same floor, if needed
         ArrayList<Node> elevatorNodes = new ArrayList<Node>();
         for (Edge edge : n.getEdges()) {
           elevatorNodes.add(edge.getStartNode());
@@ -51,12 +54,36 @@ public class BFS implements IPathfinding {
             visited.add(node); // vators as well
             parentMap.put(node, n);
             queue.add(node);
+            nodeChosen = true;
+            break;
+          }
+        }
+        int i = 1;
+        ArrayList<Node> nodesRevisited = new ArrayList<Node>();
+        while (!nodeChosen) { // sometimes, elevators only lead to other elevators. In this case,
+          elevatorNodes.clear(); // we need to go one node back, and look for another unvisited one.
+          for (Edge ex : visited.get(visited.size() - i).getEdges()) {
+            elevatorNodes.add(ex.getStartNode());
+            elevatorNodes.add(ex.getEndNode());
+          }
+          nodesRevisited.add(visited.get(visited.size() - i)); // list of nodes we go back to
+          i++;
+          for (Node node : elevatorNodes) {
+            if (!visited.contains(node)
+                && !node.getLocation().getNodeType().equalsIgnoreCase("ELEV")) {
+              visited.addAll(nodesRevisited);
+              visited.add(node);
+              parentMap.put(node, n);
+              queue.add(node);
+              nodeChosen = true;
+              break;
+            }
           }
         }
       }
       if (n.getLocation().getNodeType().equalsIgnoreCase("STAI")
-          && !n.getFloor()
-              .equalsIgnoreCase(endFloor)) { // branch to send to correct floor, if needed
+          && !n.getFloor().equalsIgnoreCase(endFloor)
+          && !nodeChosen) { // branch to send to correct floor, if needed
         ArrayList<Node> stairNodes = new ArrayList<Node>();
         for (Edge edge : n.getEdges()) {
           stairNodes.add(edge.getStartNode());
@@ -67,12 +94,14 @@ public class BFS implements IPathfinding {
             visited.add(node);
             parentMap.put(node, n);
             queue.add(node);
+            nodeChosen = true;
+            break;
           }
         }
       }
       if (n.getLocation().getNodeType().equalsIgnoreCase("STAI")
-          && n.getFloor()
-              .equalsIgnoreCase(endFloor)) { // branch to send to correct floor, if needed
+          && n.getFloor().equalsIgnoreCase(endFloor)
+          && !nodeChosen) { // branch to send to same floor, if needed
         ArrayList<Node> stairNodes = new ArrayList<Node>();
         for (Edge edge : n.getEdges()) {
           stairNodes.add(edge.getStartNode());
@@ -83,40 +112,79 @@ public class BFS implements IPathfinding {
             visited.add(node);
             parentMap.put(node, n);
             queue.add(node);
+            nodeChosen = true;
+            break;
+          }
+        }
+        int i = 1;
+        ArrayList<Node> nodesRevisited = new ArrayList<Node>();
+        while (!nodeChosen) { // sometimes, stairs only lead to other stairs. In this case,
+          stairNodes.clear(); // we need to go one node back, and look for another unvisited one.
+          for (Edge ex : visited.get(visited.size() - i).getEdges()) {
+            stairNodes.add(ex.getStartNode());
+            stairNodes.add(ex.getEndNode());
+          }
+          nodesRevisited.add(visited.get(visited.size() - i)); // list of nodes we go back to
+          i++;
+          for (Node node : stairNodes) {
+            if (!visited.contains(node)
+                && !node.getLocation().getNodeType().equalsIgnoreCase("STAI")) {
+              visited.addAll(nodesRevisited);
+              visited.add(node);
+              parentMap.put(node, n);
+              queue.add(node);
+              nodeChosen = true;
+              break;
+            }
           }
         }
       }
-      for (Edge edge : n.getEdges()) {
+      for (Edge edge :
+          n.getEdges()) { // normal situation choice branches, one for end nodes, one for start
         Node neighbor = edge.getEndNode();
         if (!visited.contains(neighbor)
             && n.getFloor().equalsIgnoreCase(neighbor.getFloor())
-            && !neighbor.equals(n)) {
+            && !neighbor.equals(n)
+            && !nodeChosen) {
           visited.add(neighbor);
           parentMap.put(neighbor, n);
           queue.add(neighbor);
-          System.out.println();
-          System.out.println(
-              "added node "
-                  + neighbor.getLocation().getShortName()
-                  + " on floor "
-                  + neighbor.getFloor());
+          nodeChosen = true;
         }
         if (!visited.contains(edge.getStartNode())
             && n.getFloor().equalsIgnoreCase(edge.getStartNode().getFloor())
-            && !n.equals(edge.getStartNode())) {
+            && !n.equals(edge.getStartNode())
+            && !nodeChosen) {
           visited.add(edge.getStartNode());
           parentMap.put(edge.getStartNode(), n);
           queue.add(edge.getStartNode());
-          System.out.println();
-          System.out.println(
-              "added node "
-                  + neighbor.getLocation().getShortName()
-                  + " on floor "
-                  + neighbor.getFloor());
+          nodeChosen = true;
+        }
+      }
+      if (queue.isEmpty() && !n.equals(target)) { // if we run into a dead end
+        int i = 1;
+        ArrayList<Node> nodesRevisited = new ArrayList<Node>();
+        ArrayList<Node> backupNodes = new ArrayList<Node>();
+        while (!nodeChosen) {
+          for (Edge ex : visited.get(visited.size() - i).getEdges()) {
+            backupNodes.add(ex.getStartNode());
+            backupNodes.add(ex.getEndNode());
+          }
+          nodesRevisited.add(visited.get(visited.size() - i));
+          i++;
+          for (Node node : backupNodes) {
+            if (!visited.contains(node) && node.getFloor().equalsIgnoreCase(n.getFloor())) {
+              visited.addAll(nodesRevisited);
+              visited.add(node);
+              parentMap.put(node, n);
+              queue.add(node);
+              nodeChosen = true;
+              break;
+            }
+          }
         }
       }
     }
-    // no path found
     return visited;
   }
 }
