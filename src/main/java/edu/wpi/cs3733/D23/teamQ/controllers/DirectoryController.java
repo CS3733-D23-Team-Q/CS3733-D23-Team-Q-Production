f93d37dcd7cea4759646ce667b4f001b822b4607
Kenyon Coleman <kenyon.coleman15@gmail.com>
2023-04-22 21:56:49 -0400

    changes

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/DirectoryController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/DirectoryController.java
@@ -1,18 +1,28 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
-import edu.wpi.cs3733.D23.teamQ.db.Qdb;
 import edu.wpi.cs3733.D23.teamQ.db.obj.Account;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import edu.wpi.cs3733.D23.teamQ.db.obj.ProfileImage;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.input.MouseEvent;
-import javafx.util.Callback;
+import javafx.scene.image.Image;
 
 public class DirectoryController {
+    @FXML TableView<EmployeeData> directoryTable;
+    @FXML TableColumn<EmployeeData, Image> profileImageColumn;
+    @FXML TableColumn<EmployeeData, String> nameColumn;
+    @FXML TableColumn<EmployeeData, String> titleColumn;
+    @FXML TableColumn<EmployeeData, String> statusColumn;
+
+    public static class EmployeeData {
+        private final Account account;
+        private final ProfileImage profileImage;
+
+        public EmployeeData(Account account, ProfileImage profileImage) {
+            this.account = account;
+            this.profileImage = profileImage;
+        }
+    }
+
 
 }
