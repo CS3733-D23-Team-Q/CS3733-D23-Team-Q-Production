5308d90c2011eae8e272788202ea24c12c109b8c
Kenyon Coleman <kenyon.coleman15@gmail.com>
2023-04-24 13:33:29 -0400

    changes

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
@@ -8,13 +8,15 @@ import edu.wpi.cs3733.D23.teamQ.db.obj.FurnitureRequest;
 import edu.wpi.cs3733.D23.teamQ.db.obj.ServiceRequest;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
+import java.util.Optional;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ListServiceRequestController {
   @FXML TableView<ServiceRequest> yourRequestsTable;
@@ -33,7 +35,7 @@ public class ListServiceRequestController {
   @FXML TableColumn<ServiceRequest, String> assignedRequestInstructions;
   @FXML TableColumn<ServiceRequest, String> assignedRequestRequester;
   @FXML TableColumn<ServiceRequest, String> assignedRequestDate;
-  @FXML TableColumn<ServiceRequest, MFXComboBox> progressDropdownColumn;
+  @FXML TableColumn<ServiceRequest, MFXComboBox<ServiceRequest.Progress>> progressDropdownColumn;
 
   private static FlowerRequest flowerRequest;
   private static ConferenceRequest conferenceRequest;
@@ -55,20 +57,21 @@ public class ListServiceRequestController {
   @FXML
   public void initialize() {
     String username = LoginController.getLoginUsername();
-    System.out.println(userAssignedRequests.size());
-    System.out.println(userRequestedRequests.size());
+
+    yourRequestsTable.setStyle("-fx-table-column-border-visible: false;");
+    assignedRequestTable.setStyle("-fx-table-column-border-visible: false;");
 
     yourRequestID.setCellValueFactory(new PropertyValueFactory<>("requestID"));
     yourRequestProgress.setCellValueFactory(new PropertyValueFactory<>("progress"));
 
     yourRequestLocation.setCellValueFactory(
-            cellData -> {
-              StringProperty locationProperty = new SimpleStringProperty();
-              ServiceRequest sr = cellData.getValue();
-              String location = qdb.retrieveLocation(sr.getNodeID()).getLongName();
-              locationProperty.set(location);
-              return locationProperty;
-            });
+        cellData -> {
+          StringProperty locationProperty = new SimpleStringProperty();
+          ServiceRequest sr = cellData.getValue();
+          String location = qdb.retrieveLocation(sr.getNodeID()).getLongName();
+          locationProperty.set(location);
+          return locationProperty;
+        });
 
     yourRequestInstructions.setCellValueFactory(new PropertyValueFactory<>("specialInstructions"));
 
@@ -82,25 +85,156 @@ public class ListServiceRequestController {
         });
 
     yourRequestDate.setCellValueFactory(
-            cellData -> {
-              StringProperty dateProperty = new SimpleStringProperty();
-              ServiceRequest sr = cellData.getValue();
-              String dateTime = sr.getDate().toString() + " " + sr.getTime();
-              dateProperty.set(dateTime);
-              return dateProperty;
-            });
-    //    editButtonColumn.setCellValueFactory(new PropertyValueFactory<>("editButton"));
-    //    deleteButtonColumn.setCellValueFactory(new PropertyValueFactory<>("deleteButton"));
+        cellData -> {
+          StringProperty dateProperty = new SimpleStringProperty();
+          ServiceRequest sr = cellData.getValue();
+          String dateTime = sr.getDate().toString() + " " + sr.getTime();
+          dateProperty.set(dateTime);
+          return dateProperty;
+        });
+
+    editButtonColumn.setCellFactory(
+        column -> {
+          return new TableCell<ServiceRequest, MFXButton>() {
+            private final MFXButton button = new MFXButton("Edit");
+
+            @Override
+            protected void updateItem(MFXButton item, boolean empty) {
+              super.updateItem(item, empty);
+              if (empty) {
+                setGraphic(null);
+              } else {
+                ServiceRequest serviceRequest = getTableView().getItems().get(getIndex());
+                button.setOnAction(
+                    event -> {
+                      System.out.println(
+                          "Edit button clicked for: " + serviceRequest.getRequestID());
+                    });
+                Image image = new Image(getClass().getResourceAsStream("/EditButton.png"));
+                ImageView imageView = new ImageView(image);
+                imageView.setFitHeight(20.0);
+                imageView.setFitWidth(20.0);
+                button.setText("");
+                button.setGraphic(imageView);
+                button.setStyle("-fx-background-color: transparent;");
+                setGraphic(button);
+              }
+            }
+          };
+        });
+
+    deleteButtonColumn.setCellFactory(
+        column -> {
+          return new TableCell<ServiceRequest, MFXButton>() {
+            private final Button button = new MFXButton("View Profile");
+
+            @Override
+            protected void updateItem(MFXButton item, boolean empty) {
+              super.updateItem(item, empty);
+              if (empty) {
+                setGraphic(null);
+              } else {
+                ServiceRequest serviceRequest = getTableView().getItems().get(getIndex());
+                button.setOnAction(
+                    event -> {
+                      Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                      alert.setTitle("Confirmation");
+                      alert.setHeaderText("Delete Service Request");
+                      alert.setContentText("Are you sure you want to delete this service request?");
+                      alert.initOwner(button.getScene().getWindow());
+
+                      Optional<ButtonType> result = alert.showAndWait();
+                      if (result.isPresent() && result.get() == ButtonType.OK) {
+                        qdb.deleteServiceRequest(serviceRequest.getRequestID());
+                        yourRequestsTable.getItems().remove(serviceRequest);
+                        yourRequestsTable.refresh();
+                      }
+                    });
+                Image image = new Image(getClass().getResourceAsStream("/DeleteButton.png"));
+                ImageView imageView = new ImageView(image);
+                imageView.setFitHeight(20.0);
+                imageView.setFitWidth(20.0);
+                button.setText("");
+                button.setGraphic(imageView);
+                button.setStyle("-fx-background-color: transparent;");
+                setGraphic(button);
+              }
+            }
+          };
+        });
+
     yourRequestsTable.setItems(userRequestedRequests);
 
     // Set data for assignedRequestTable
     assignedRequestID.setCellValueFactory(new PropertyValueFactory<>("requestID"));
-    assignedRequestLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
-    assignedRequestInstructions.setCellValueFactory(new PropertyValueFactory<>("instructions"));
-    assignedRequestRequester.setCellValueFactory(new PropertyValueFactory<>("requester"));
-    assignedRequestDate.setCellValueFactory(new PropertyValueFactory<>("date"));
-    //    progressDropdownColumn.setCellValueFactory(new
-    // PropertyValueFactory<>("progressDropdown"));
+
+    assignedRequestLocation.setCellValueFactory(
+        cellData -> {
+          StringProperty locationProperty = new SimpleStringProperty();
+          ServiceRequest sr = cellData.getValue();
+          String location = qdb.retrieveLocation(sr.getNodeID()).getLongName();
+          locationProperty.set(location);
+          return locationProperty;
+        });
+
+    assignedRequestInstructions.setCellValueFactory(
+        new PropertyValueFactory<>("specialInstructions"));
+    assignedRequestRequester.setCellValueFactory(
+        cellData -> {
+          StringProperty requesterProperty = new SimpleStringProperty();
+          ServiceRequest sr = cellData.getValue();
+          String requester = sr.getRequester().getUsername();
+          requesterProperty.set(requester);
+          return requesterProperty;
+        });
+
+    assignedRequestDate.setCellValueFactory(
+        cellData -> {
+          StringProperty dateProperty = new SimpleStringProperty();
+          ServiceRequest sr = cellData.getValue();
+          String dateTime = sr.getDate().toString() + " " + sr.getTime();
+          dateProperty.set(dateTime);
+          return dateProperty;
+        });
+
+    progressDropdownColumn.setCellFactory(
+        column -> {
+          return new TableCell<ServiceRequest, MFXComboBox<ServiceRequest.Progress>>() {
+            private final MFXComboBox<ServiceRequest.Progress> comboBox = new MFXComboBox<>();
+
+            {
+              // Set up the options for the ComboBox
+              comboBox.setPrefHeight(20);
+              comboBox.getItems().addAll(ServiceRequest.Progress.values());
+              // Set the value of the ComboBox when changed
+              comboBox.setOnAction(
+                  event -> {
+                    ServiceRequest serviceRequest = getTableView().getItems().get(getIndex());
+                    serviceRequest.setProgress(comboBox.getValue());
+                    // Update the ServiceRequest object and refresh the TableView
+                    // with the new value
+                    // You may need to call a method to update the progress
+                    // in your application logic
+                    yourRequestsTable.refresh();
+                  });
+            }
+
+            @Override
+            protected void updateItem(MFXComboBox<ServiceRequest.Progress> item, boolean empty) {
+              super.updateItem(item, empty);
+              if (empty) {
+                setGraphic(null);
+              } else {
+                ServiceRequest serviceRequest = getTableView().getItems().get(getIndex());
+                // Set the value of the ComboBox based on the progress
+                // value of the ServiceRequest object
+                comboBox.setValue(serviceRequest.getProgress());
+                setGraphic(comboBox);
+              }
+            }
+          };
+        });
+
     assignedRequestTable.setItems(userAssignedRequests);
   }
 
