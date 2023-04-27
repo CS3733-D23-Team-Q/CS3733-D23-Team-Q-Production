e57087b0eed9b9b99adf159a895cdc338228fe51
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-27 14:10:07 -0400

    full alerts and home setup, minus reactive time

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/CreateAlertController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/CreateAlertController.java
@@ -30,57 +30,57 @@ public class CreateAlertController {
 
   @FXML
   public void blueClicked() {
-    input.setText("Code Blue: Cardiac/Respiratory arrest in room ___");
+    input.setText("Code Blue: Cardiac/Respiratory arrest in room ___.");
   }
 
   @FXML
   public void redClicked() {
     input.setText(
-        "Code Red: Fire Emergency on floor ___. Please evacuate all patients immediately");
+        "Code Red: Fire Emergency on floor ___. Please evacuate all patients immediately.");
   }
 
   @FXML
   public void blackClicked() {
     input.setText(
-        "Code Black: Bomb Threat/Active Shooter on floor ___. Please evacuate all patients immediately");
+        "Code Black: Bomb Threat/Active Shooter on floor ___. Please evacuate all patients immediately.");
   }
 
   @FXML
   public void grayClicked() {
     input.setText(
-        "Code Gray: Severe weather event warning. Please move away from all windows and external doors, and make your way to lower level 1");
+        "Code Gray: Severe weather event warning. Please move away from all windows and external doors, and make your way to lower level 1.");
   }
 
   @FXML
   public void yellowClicked() {
-    input.setText("Code Yellow: Missing patient alert. Last seen in room ___");
+    input.setText("Code Yellow: Missing patient alert. Last seen in room ___.");
   }
 
   @FXML
   public void orangeClicked() {
-    input.setText("Code Orange: Hazardous material release in room ___. Please avoid the area");
+    input.setText("Code Orange: Hazardous material release in room ___. Please avoid the area.");
   }
 
   @FXML
   public void pinkClicked() {
-    input.setText("Code Pink: Pediatric emergency in room ___");
+    input.setText("Code Pink: Pediatric emergency in room ___.");
   }
 
   @FXML
   public void purpleClicked() {
     input.setText(
-        "Code Purple: Violent patient/visitor in room ___. Please evacuate the area immediately");
+        "Code Purple: Violent patient/visitor in room ___. Please evacuate the area immediately.");
   }
 
   @FXML
   public void greenClicked() {
-    input.setText("Code Green: External mass casualty event. All staff on high alert");
+    input.setText("Code Green: External mass casualty event. All staff on high alert.");
   }
 
   @FXML
   public void silverClicked() {
     input.setText(
-        "Code Silver: Security threat on floor ___. Please evacuate the area immediately");
+        "Code Silver: Security threat on floor ___. Please evacuate the area immediately.");
   }
 
   @FXML
