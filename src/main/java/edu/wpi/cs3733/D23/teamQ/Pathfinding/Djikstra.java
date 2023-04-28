fb81ee1e0f75dbf3fb4592d5a1bdeedef967bdb6
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-28 17:33:48 -0400

    Code cleanup

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Djikstra.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Djikstra.java
@@ -13,14 +13,10 @@ public class Djikstra implements IPathfinding {
     closedList.add(start);
     while (!openList.isEmpty()) {
       Node current = openList.get(0);
-      if (current == null) {
-        return path;
-      }
+      path.add(current);
       if (current.equals(end)) {
-        path.add(current);
         return path;
       }
-      // write in elevator specifications
       double lowestLocalCost = 10000000.0;
       boolean nextChosen = false;
       Node chosen = null;
@@ -28,19 +24,12 @@ public class Djikstra implements IPathfinding {
               && !current.getFloor().equalsIgnoreCase(end.getFloor()) // floors
           || current.getLocation().getNodeType().equalsIgnoreCase("STAI")
               && !current.getFloor().equalsIgnoreCase(end.getFloor())) {
-        System.out.println(
-            "Found an elevator to a diff floor at " + current + " with end " + end.getFloor());
-        ArrayList<Node> elevatorTargets = new ArrayList<Node>();
-        for (Edge these : current.getEdges()) {
-          elevatorTargets.add(these.getStartNode());
-          elevatorTargets.add(these.getEndNode());
-        }
+        ArrayList<Node> elevatorTargets = getConnections(current);
         for (Node node : elevatorTargets) {
           if (node.getFloor().equalsIgnoreCase(end.getFloor()) && !node.equals(current)) {
             Node chosenElev = node;
             openList.add(chosenElev);
             openList.remove(current);
-            path.add(current);
             closedList.add(current);
             nextChosen = true;
             break;
@@ -53,16 +42,13 @@ public class Djikstra implements IPathfinding {
           if (!closedList.contains(thisOne.getStartNode())
               && thisOne.getStartNode().getFloor().equalsIgnoreCase(current.getFloor())
               && !thisOne.getStartNode().equals(current)
-              && !path.contains(thisOne.getStartNode())
-          //   && thisOne.getStartNode()
-          ) {
+              && !closedList.contains(thisOne.getStartNode())) {
             double xDist = Math.abs(end.getXCoord() - thisOne.getStartNode().getXCoord());
             double yDist = Math.abs(end.getYCoord() - thisOne.getStartNode().getYCoord());
             double weight = Math.sqrt(xDist * xDist + yDist * yDist);
             if (weight < lowestLocalCost) {
               lowestLocalCost = weight;
               chosen = thisOne.getStartNode();
-              System.out.println();
             }
           } else if (!closedList.contains(
                   thisOne.getEndNode()) // main choice block for end nodes, used to cacl
@@ -75,15 +61,11 @@ public class Djikstra implements IPathfinding {
             if (weight < lowestLocalCost) {
               lowestLocalCost = weight;
               chosen = thisOne.getEndNode();
-              System.out.println();
             }
           }
         }
       }
-      if (!path.contains(current)) {
-        path.add(current);
-      }
-      if (chosen == null) {
+      /* if (chosen == null) {
         ArrayList<Node> altNodes = new ArrayList<Node>(); // backup block
         for (Edge edge : closedList.get(closedList.size() - 1).getEdges()) {
           altNodes.add(edge.getStartNode());
@@ -94,33 +76,26 @@ public class Djikstra implements IPathfinding {
             chosen = node2;
           }
         }
-      }
-      if (chosen != null
-              && chosen.getLocation().getNodeType().equalsIgnoreCase("STAI") // test statement
-          || chosen != null
-              && chosen.getLocation().getNodeType().equalsIgnoreCase("ELEV")) { // block
-        System.out.println(
-            "The chosen node was an elevator/stair at node "
-                + chosen.getNodeID()
-                + " and it was a "
-                + chosen.getLocation().getNodeType()
-                + " from current "
-                + current);
-      }
+      }*/
       if (chosen == null) {
-        System.out.println("COUNTERMEAUSRES LULW");
-      }
-      if (!path.contains(current)) {
-        path.add(current);
+        System.out.println("COUNTERMEAUSRES LULW AT NODE " + current);
+        return path;
       }
       if (!openList.contains(chosen)) {
         openList.add(chosen);
       }
-      if (openList.contains(current)) openList.remove(current);
+      openList.remove(current);
       if (!closedList.contains(current)) closedList.add(current);
-      System.out.println();
-      System.out.println("Added node " + current.getNodeID() + " to closedList");
     }
     return path;
+  }
+
+  public ArrayList<Node> getConnections(Node current) {
+    ArrayList<Node> nodesAvailable = new ArrayList<Node>();
+    for (Edge edgePath : current.getEdges()) {
+      nodesAvailable.add(edgePath.getStartNode());
+      nodesAvailable.add(edgePath.getEndNode());
+    }
+    return nodesAvailable;
   }
 }
