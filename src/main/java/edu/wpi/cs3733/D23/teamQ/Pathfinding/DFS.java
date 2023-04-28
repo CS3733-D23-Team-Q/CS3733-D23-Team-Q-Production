10383d4c45dbd499faa1e74159868cc96dd415c3
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-28 15:56:44 -0400

    more

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/DFS.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/DFS.java
@@ -25,6 +25,23 @@ public class DFS implements IPathfinding {
         nodesAvailable.add(edgePath.getEndNode());
       }
       if (current.getLocation().getNodeType().equalsIgnoreCase("ELEV") // block used to guide
+              && !current.getFloor().equalsIgnoreCase(end.getFloor())
+          || current.getLocation().getNodeType().equalsIgnoreCase("STAI")
+              && !current.getFloor().equalsIgnoreCase(end.getFloor())) { // path onto correct floor
+        for (Node node : nodesAvailable) {
+          if (node.getFloor().equalsIgnoreCase(end.getFloor())
+              && !path.contains(node)
+              && !node.equals(current)
+              && !nextChosen // find a elevator
+          ) {
+            openList.add(node);
+            openList.remove(current);
+            visitedList.add(current);
+            nextChosen = true;
+          }
+        }
+      }
+      if (current.getLocation().getNodeType().equalsIgnoreCase("ELEV") // block used to guide
               && current.getFloor().equalsIgnoreCase(end.getFloor())
           || current.getLocation().getNodeType().equalsIgnoreCase("STAI")
               && current.getFloor().equalsIgnoreCase(end.getFloor())) { // thru stair/elev
@@ -85,6 +102,39 @@ public class DFS implements IPathfinding {
         ArrayList<Node> previousNodes = new ArrayList<Node>();
         i++;
         if (i > 2000) {
+          ArrayList<Node> fixPath = new ArrayList<Node>();
+          ArrayList<Node> targetNodes = getConnections(end);
+          for (Node node : targetNodes) {
+            if (path.contains(node)) {
+              Node connectedNode = node;
+              for (int g = path.size() - 1; g > 0; g--) {
+                fixPath.add(path.get(g));
+                Node pathNode = path.get(g);
+                if (pathNode.equals(connectedNode)) {
+                  path.addAll(fixPath);
+                  path.add(end);
+                  return path;
+                }
+              }
+            }
+          }
+          for (Node node2 : targetNodes) {
+            ArrayList<Node> retracedPath = new ArrayList<Node>();
+            ArrayList<Node> connections2 = getConnections(node2);
+            for (Node node3 : connections2) {
+              if (path.contains(node3)) {
+                for (int g = path.size() - 1; g > 0; g--) {
+                  retracedPath.add(path.get(g));
+                  if (retracedPath.get(g).equals(node3)) {
+                    path.addAll(retracedPath);
+                    path.add(node2);
+                    path.add(end);
+                    return path;
+                  }
+                }
+              }
+            }
+          }
           return path;
         }
         for (Edge previousEdge : previous.getEdges()) {
@@ -102,21 +152,20 @@ public class DFS implements IPathfinding {
             path.addAll(revisitedNodes);
             visitedList.add(current);
             nextChosen = true;
-            /*   System.out.println();
-            System.out.println(
-                " FROM NON-CHOSEN BRANCH ADDED FROM NODE "
-                    + current.getNodeID()
-                    + " WITH PATH "
-                    + revisitedNodes
-                    + " AND SIZE "
-                    + revisitedNodes.size()
-                    + " TO "
-                    + previousNode.getNodeID());*/
             break;
           }
         }
       }
     }
     return path;
+  }
+
+  public ArrayList<Node> getConnections(Node current) {
+    ArrayList<Node> nodesAvailable = new ArrayList<Node>();
+    for (Edge edgePath : current.getEdges()) {
+      nodesAvailable.add(edgePath.getStartNode());
+      nodesAvailable.add(edgePath.getEndNode());
+    }
+    return nodesAvailable;
   }
 }
