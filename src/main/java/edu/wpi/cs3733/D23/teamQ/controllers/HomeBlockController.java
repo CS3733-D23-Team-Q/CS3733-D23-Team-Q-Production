d5cb366b700eb51422dc6cf4fc65534beb2a105e
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-29 19:26:10 -0400

    fixed

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/HomeBlockController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/HomeBlockController.java
@@ -4,11 +4,16 @@ import edu.wpi.cs3733.D23.teamQ.App;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class HomeBlockController {
   @FXML AnchorPane anchor;
@@ -30,7 +35,8 @@ public class HomeBlockController {
     homeIcon.setImage(new Image(App.class.getResourceAsStream("Home.png")));
   }
 
-  public void homeClicked() {
+  public void homeClicked() throws URISyntaxException {
+    alertSound("Code Blue");
     mc.showAll();
     Navigation.navigate(Screen.HOME);
   }
@@ -43,5 +49,24 @@ public class HomeBlockController {
 
   public void setMCController(MenuController MC) {
     mc = MC;
+  }
+
+  public void alertSound(String message) throws URISyntaxException {
+    URI path = getClass().getResource("/alert.wav").toURI();
+
+    if (message.contains("Code Blue")) path = getClass().getResource("/Blue.wav").toURI();
+    if (message.contains("Code Red")) path = getClass().getResource("/Red.wav").toURI();
+    if (message.contains("Code Black")) path = getClass().getResource("/Black.wav").toURI();
+    if (message.contains("Code Gray")) path = getClass().getResource("/Gray.wav").toURI();
+    if (message.contains("Code Yellow")) path = getClass().getResource("/Yellow.wav").toURI();
+    if (message.contains("Code Orange")) path = getClass().getResource("/Orange.wav").toURI();
+    if (message.contains("Code Pink")) path = getClass().getResource("/Pink.wav").toURI();
+    if (message.contains("Code Purple")) path = getClass().getResource("/Purple.wav").toURI();
+    if (message.contains("Code Green")) path = getClass().getResource("/Green.wav").toURI();
+    if (message.contains("Code Silver")) path = getClass().getResource("/Silver.wav").toURI();
+
+    Media media = new Media(new File(path).toURI().toString());
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.play();
   }
 }
