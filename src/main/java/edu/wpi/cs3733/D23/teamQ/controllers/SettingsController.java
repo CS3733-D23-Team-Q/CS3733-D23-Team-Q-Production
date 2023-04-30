adef05db883cef75b2fe22a6621d5c543c00a826
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2023-04-30 12:28:38 -0400

    need to help josh

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SettingsController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SettingsController.java
@@ -13,6 +13,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.paint.Color;
 
+import java.util.Set;
+
 public class SettingsController {
 
   Qdb qdb = Qdb.getInstance();
@@ -65,10 +67,12 @@ public class SettingsController {
               @Override
               public void changed(ObservableValue observable, Object oldValue, Object newValue) {
                 Settings.Voice voice = null;
+                //Settings settings = new Settings(LoginController.getLoginUsername(), qdb.retrieveSettings(LoginController.getLoginUsername()).isTwoFactor(),qdb.retrieveSettings(LoginController.getLoginUsername()).isSound(),qdb.retrieveSettings(LoginController.getLoginUsername()).getAlgorithm(),voice));
+
                 if (newValue.toString().equals("Female")) voice = Settings.Voice.FEMALE;
                 if (newValue.toString().equals("Male")) voice = Settings.Voice.MALE;
                 if (newValue.toString().equals("Snoop Dogg")) voice = Settings.Voice.SNOOP;
-                qdb.retrieveSettings(LoginController.getLoginUsername()).setVoice(voice);
+                //qdb.updateSettingsRow(LoginController.getLoginUsername(), voice);
                 System.out.println("Setting Voice to" + voice);
               }
             });
